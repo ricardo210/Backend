dbb21e5741ded970dc0106e2e8696d8664b3b1ac
ricardo210 <ricardo21095@gmail.com>
2016-03-21 00:09:34 -0600

    fisrst commit in users

---
+++ schemas/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var UserSchema = new mongoose.Schema({
+  username : {type: String, unique: true, required: true},
+  password : String,
+  code: {type: String, unique: true, required: true},
+  name : String,
+  cell : String,
+  address : String,
+  email : String,
+  scope : [String]
+});
+
+UserSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('User', UserSchema);
